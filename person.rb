be9796ff98086bb6cfaf7d59ab818411a818c445
DaveWexler <dwexler@gmail.com>
2017-03-19 04:12:07 -0400

    finished output, need tests and readme

---
+++ person.rb
@@ -0,0 +1,68 @@
+require 'pry'
+
+class Person
+  require 'time'
+
+  attr_accessor :firstname, :lastname, :middleinit, :gender, :dob, :color
+  @@all = []
+
+  def initialize(attrs)
+    @firstname = attrs[:firstname]
+    @lastname = attrs[:lastname]
+    @middleinit = attrs[:middleinit]
+    @gender = attrs[:gender]
+    @dob = attrs[:dob].gsub("-", "/").gsub("\n", "")
+    @color = attrs[:color]
+    self.fix_gender
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def convert_dob_to_datetime
+    self.dob = Date.strptime(self.dob, '%m/%d/%Y')
+  end
+
+  def convert_datetime_to_dob
+    date_str = self.dob.strftime("%m/%d/%Y")
+    date_arr = date_str.split("/")
+    date_arr[0..1].each do |n|
+      if n[0] == "0"
+        n[0] = ""
+      end
+    end
+    self.dob = date_arr.join("/")
+  end
+
+  def fix_gender
+    self.gender[0].downcase == "m" ? self.gender = "Male" : self.gender = "Female"
+  end
+
+  def self.sort_gender_lastname
+    self.all.sort!{ |a,b| (a.gender == b.gender) ? a.lastname <=> b.lastname : a.gender <=> b.gender }
+  end
+
+  def self.sort_dob_lastname
+    self.all.each { |p| p.convert_dob_to_datetime }
+    self.all.sort!{ |a,b| (a.dob == b.dob) ? a.lastname <=> b.lastname : a.dob <=> b.dob }
+    self.all.each { |p| p.convert_datetime_to_dob}
+  end
+
+  def self.sort_lastname_desc
+    self.all.sort_by!{ |person| person.lastname}.reverse!
+  end
+
+  def self.format_all_dob_for_output
+    Person.all.each do |p|
+      p.convert_datetime_to_dob
+    end
+  end
+
+  def self.date_to_slashes
+    self.all.each do |p|
+      p.dob = p.dob.gsub("-", "/")
+    end
+  end
+end
---
+++ person_organizer.rb
@@ -0,0 +1,69 @@
+require_relative 'person'
+require 'pry'
+
+file1 = File.readlines("file1.txt")
+file2 = File.readlines("file2.txt")
+file3 = File.readlines("file3.txt")
+
+def create_person_objects(file)
+  file.each do |line|
+    if line.include?(",")
+      attrs = line.split(", ")
+      Person.new(comma_attrs(attrs))
+    elsif line.include?("|")
+      attrs = line.split(" | ")
+      Person.new(pipe_attrs(attrs))
+    else
+      attrs = line.split(" ")
+      Person.new(space_attrs(attrs))
+    end
+  end
+end
+
+def comma_attrs(array)
+  {lastname: array[0], firstname: array[1], middleinit: nil, gender: array[2], dob: array[4], color: array[3]}
+end
+def pipe_attrs(array)
+  {lastname: array[0], firstname: array[1], middleinit: array[2], gender: array[3], dob: array[5], color: array[4]}
+end
+def space_attrs(array)
+  {lastname: array[0], firstname: array[1], middleinit: array[2], gender: array[3], dob: array[4], color: array[5]}
+end
+
+
+def output_1
+  Person.sort_gender_lastname
+  puts "Output 1:"
+  output_people
+  puts "\n"
+end
+
+def output_2
+  Person.sort_dob_lastname
+  puts "Output 2:"
+  output_people
+  puts "\n"
+end
+
+def output_3
+  Person.sort_lastname_desc
+  puts "Output 3:"
+  output_people
+  puts "\n"
+end
+
+def output_people
+  Person.all.each do |p|
+    puts "#{p.lastname} #{p.firstname} #{p.gender} #{p.dob} #{p.color}"
+  end
+end
+
+create_person_objects(file1)
+create_person_objects(file2)
+create_person_objects(file3)
+
+Person.date_to_slashes
+
+output_1
+output_2
+output_3
